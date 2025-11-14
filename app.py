c5c031d2a8132938ea72df8a1d477d58e896e3f2
ardabieva2006-ship-it <ardabieva2006@gmail.com>
2025-11-14 20:28:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pickle
 import numpy as np
 import pandas as pd
 
+# -----------------------------
+# LOAD MODEL AND FEATURES
+# -----------------------------
 @st.cache_resource
 def load_model():
     with open("risk_model.pkl", "rb") as f:
@@ -11,7 +14,7 @@ def load_model():
 
 model = load_model()
 
-# FEATURES IN EXACT ORDER:
+# FEATURES IN EXACT ORDER FOR MODEL
 feature_cols = [
     "open", "high", "low", "close", "adj_close", "volume",
     "return", "spread", "return_5d", "volume_5d",
@@ -21,22 +24,49 @@ feature_cols = [
 ]
 
 st.title("Investment Risk Predictor")
+st.write("Enter just 5 values ​​- everything else is calculated automatically.")
 
-st.write("Enter all 18 parameters, the model is trained on them:")
+open_p = st.number_input("Open Price", value=100.0)
+high_p = st.number_input("High Price", value=101.0)
+low_p = st.number_input("Low Price", value=99.0)
+close_p = st.number_input("Close Price", value=100.5)
+volume_p = st.number_input("Volume", value=1_000_000)
 
-inputs = {}
+ret = (close_p - open_p) / open_p if open_p != 0 else 0
+spread = close_p - open_p
 
-for col in feature_cols:
-    inputs[col] = st.number_input(col, value=0.0, format="%.6f")
+return_5d = 0       
+volume_5d = volume_p  
+
+sma_5 = 0
+sma_10 = 0
+sma_ratio = 0
+
+momentum_10 = 0    
+
+range_ = high_p - low_p
+
+return_lag1 = 0     
+return_lag2 = 0
+
+volatility_60 = 0   
+
+
+X = pd.DataFrame([[
+    open_p, high_p, low_p, close_p, close_p, volume_p,
+    ret, spread, return_5d, volume_5d,
+    sma_5, sma_10, sma_ratio, momentum_10,
+    range_, return_lag1, return_lag2,
+    volatility_60
+]], columns=feature_cols)
+
 
 if st.button("Predict Risk Level"):
-    X = pd.DataFrame([[inputs[col] for col in feature_cols]], columns=feature_cols)
-
     pred = model.predict(X)[0]
 
     if pred == 0:
-        st.success("Low Risk(0)")
+        st.success("LOW RISK (0)")
     elif pred == 1:
-        st.warning("Medium Risk(1)")
+        st.warning("MEDIUM RISK (1)")
     else:
-        st.error("High Risk(2)")
+        st.error("HIGH RISK (2)")
